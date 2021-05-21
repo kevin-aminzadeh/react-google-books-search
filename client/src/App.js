55567581cfc509ffc5384403eda8147506ffd855
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-21 17:40:05 +0930

    feat: Implement landing page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
+import Container from "./components/container/Container";
+import Hero from "./components/hero/Hero";
+import SearchBar from "./components/searchbar/SearchBar";
+import ResultsContainer from "./components/resultsContainer/ResultsContainer";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="App ">
+      <Navbar />
+      <Container>
+        <Hero />
+        <SearchBar />
+        <ResultsContainer />
+      </Container>
     </div>
   );
 }
