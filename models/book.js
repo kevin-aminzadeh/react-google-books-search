60b117ff6a14c5394b64d583aef8588d801b429a
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-21 18:17:29 +0930

    feat: Implement Book model

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  subtitle: { type: String, required: false },
+  authors: { type: [String], required: true },
+  synopsis: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
