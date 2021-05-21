61d5bb7fddbd8bbec3a900f831f2a97bdd7c72c3
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-21 17:38:25 +0930

    refactor: Update component styles

--- client/src/components/searchbar/SearchBar.js
+++ client/src/components/searchbar/SearchBar.js
@@ -1,35 +1,33 @@
 import React from "react";
-import Container from "../container/Container";
+import Section from "../section/Section";
 
 function SearchBar() {
   return (
-    <Container>
-      <div className="row">
+    <Section id="search" className="mb-4">
+      <div className="row mb-3">
         <div className="col">
           <h2>Book Search</h2>
         </div>
       </div>
       <div className="row">
-        <div className="col">
-          <form>
-            <div className="mb-3">
-              <label htmlFor="searchInput" className="form-label">
-                Books
-              </label>
+        <div className="col px-0">
+          <form className="w-100">
+            <div className="input-group mb-3">
               <input
                 type="text"
-                className="form-control"
-                id="searchInput"
-                aria-describedby="searchHelp"
+                className="form-control border-0 py-3 shadow-sm"
+                placeholder="Search For Books"
+                aria-label="Book Name"
+                aria-describedby="search-btn"
               />
-              <div id="searchHelp" className="form-text visually-hidden">
-                Search for books on Google Books.
-              </div>
+              <button className="btn btn-primary" id="search-btn">
+                Search
+              </button>
             </div>
           </form>
         </div>
       </div>
-    </Container>
+    </Section>
   );
 }
 
