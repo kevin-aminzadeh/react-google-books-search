ef249edfa665aecee7e35f816e7360b5457604a4
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-21 17:36:59 +0930

    refactor: Utilize Section component for better semantics

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Container from "../container/Container";
+import Section from "../section/Section";
 
 function Hero() {
   return (
-    <Container>
-      <div className="row text-center">
+    <Section id="hero">
+      <div className="row text-center mb-5">
         <div className="col">
           <h1>(React) Google Books Search</h1>
           <p className="lead">Search for and Save Books of Interest</p>
         </div>
       </div>
-    </Container>
+    </Section>
   );
 }
 
