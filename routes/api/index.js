f39783c940a58db520aa76058fc1725e8e99934b
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-21 18:43:19 +0930

    feat: Impelement API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Handle "/api/books" Route Logic
+router
+  .route("/books")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Handle "/api/books/:id" Route Logic
+router.route("/books/:id").delete(booksController.remove);
 
 module.exports = router;
