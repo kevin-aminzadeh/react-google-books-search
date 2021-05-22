0359349b43473f58e4eed8a2c67f78b23ff632ee
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-23 01:28:14 +0930

    fix: Remove explicitly stated URL from socket.io config

--- client/src/utils/socketContext.js
+++ client/src/utils/socketContext.js
@@ -2,7 +2,7 @@ import React from "react";
 import io from "socket.io-client";
 
 // Open Up a New Socket Connection
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect();
 
 // Set Default Socket Context Value to "socket"
 const SocketContext = React.createContext(socket);
