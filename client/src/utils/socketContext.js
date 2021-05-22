7fd0dfe1ede49420f40f8c1acedb6d922023361e
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-23 02:02:31 +0930

    refactor: Update socket.io connection url for production

--- client/src/utils/socketContext.js
+++ client/src/utils/socketContext.js
@@ -1,8 +1,13 @@
 import React from "react";
 import io from "socket.io-client";
 
+const socketURL =
+  process.env.NODE_ENV === "production"
+    ? window.location.hostname
+    : "https://localhost:3000";
+
 // Open Up a New Socket Connection
-const socket = io.connect();
+const socket = io.connect(socketURL);
 
 // Set Default Socket Context Value to "socket"
 const SocketContext = React.createContext(socket);
