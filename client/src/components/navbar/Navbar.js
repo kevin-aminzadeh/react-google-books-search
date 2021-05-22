00e331d215b3ab100e50f4b6477b49a15c9799d5
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-22 23:47:34 +0930

    refactor: Remove unnecessary collapsing behaviour

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,22 +4,12 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+      <nav className="navbar navbar-expand navbar-dark bg-dark py-3">
         <div className="container-fluid">
           <span className="navbar-brand" href="#">
             Google Books
           </span>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
