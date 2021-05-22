e8b846686c73890c8f2b8a64e61444051a911255
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-22 16:33:22 +0930

    refactor: Update model to utilize Google Books ID values

--- models/book.js
+++ models/book.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+  _id: { type: String, required: true },
   title: { type: String, required: true },
   subtitle: { type: String, required: false },
   authors: { type: [String], required: true },
