aefe80acdc1e973888198845992a2fe546477820
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-22 17:05:17 +0930

    refactor: Update ListItem component key value to book's _id property value

--- client/src/components/listContainer/ListContainer.js
+++ client/src/components/listContainer/ListContainer.js
@@ -4,8 +4,8 @@ import ListItem from "../listItem/ListItem";
 function ListContainer(props) {
   return (
     <ul className="px-0 row justify-content-center">
-      {props.data.map((item, index) => (
-        <ListItem data={item} key={index} handleSave={props.handleSave} />
+      {props.data.map((item) => (
+        <ListItem data={item} key={item._id} handleSave={props.handleSave} />
       ))}
     </ul>
   );
