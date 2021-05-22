ea5aec2158b29be3202af8348d3968c74a686706
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-22 23:48:44 +0930

    feat: Implement Socket.IO notification system

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const moongose = require("mongoose");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// Import Routes
 const routes = require("./routes");
 
 // Define express middleware
@@ -19,6 +24,22 @@ app.use(routes);
 // Connect to DB
 moongose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
-app.listen(PORT, () => {
+// Configure Socket.io Events
+io.on("connection", (socket) => {
+  // On "bookSaved" Event,
+  socket.on("bookSaved", (bookTitle) => {
+    // Emit a "booksUpdated" Event on All Sockets With Relevant Data
+    io.sockets.emit("booksUpdated", { type: "add", item: bookTitle });
+  });
+
+  // On "bookDeleted" Event,
+  socket.on("bookDeleted", (bookTitle) => {
+    // Emit a "booksUpdated" Event on All Sockets With Relevant Data
+    io.sockets.emit("booksUpdated", { type: "delete", item: bookTitle });
+  });
+});
+
+// Start Server
+server.listen(PORT, () => {
   console.log(`🌎 ==> API server now listening on port ${PORT}!`);
 });
