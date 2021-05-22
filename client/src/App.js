f4bc1982bbf123fdabcb2adb55e3c0a96621b8ed
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-22 16:50:16 +0930

    feat: Implement savedBooks state and context

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Search from "./components/pages/search/Search";
 import SavedBooks from "./components/pages/savedBooks/SavedBooks";
 import Navbar from "./components/navbar/Navbar";
+import API from "./utils/API";
+
+import SavedBooksContext from "./utils/SavedBooksContext";
 
 function App() {
+  const [savedBooksState, setSavedBooksState] = useState({});
+
+  useEffect(() => {
+    API.getSavedBooks()
+      .then((res) => {
+        if (Object.keys(res).length) {
+          console.log(res);
+          setSavedBooksState(res);
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+
   return (
     <div className="App ">
       <Router>
         <Navbar />
-        <Route exact path={["/", "/search"]}>
-          <Search />
-        </Route>
-        <Route exact path={"/saved"}>
-          <SavedBooks />
-        </Route>
+        <Switch>
+          <SavedBooksContext.Provider value={{ ...savedBooksState }}>
+            <Route exact path={["/", "/search"]}>
+              <Search />
+            </Route>
+            <Route path={"/saved"}>
+              <SavedBooks />
+            </Route>
+          </SavedBooksContext.Provider>
+        </Switch>
       </Router>
     </div>
   );
